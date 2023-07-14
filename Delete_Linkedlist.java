d093d26147c786506b27e02cfc2e7ac0917aadf1
Wrundali shende <118574986+Wrundali31@users.noreply.github.com>
2023-07-15 01:24:51 +0530

    Update and rename DeleteL.java to Delete_Linkedlist.java

--- Delete_Linkedlist.java
+++ Delete_Linkedlist.java
@@ -1,25 +1,28 @@
 //program to remove/delete elements in linkedlist
 
 import java.util.*;            
-class DeleteL
+class Delete_Linkedlist
 {  
 public static void main(String args[])
 {
-LinkedList<String> L1=new LinkedList<>();
+//creation of linkedlist
+ LinkedList<String> L1=new LinkedList<>(); 
 L1.add("one");
 L1.add("two");
 L1.add("three");
 L1.add("four");
 L1.add("five");
 
-
+//print linkedlist named L1
 System.out.println(L1);
-L1.remove();
+//removing or deleting elements from L1
+ L1.remove();
  L1.remove("two");
 L1.remove(2);
 L1.removeFirst();
 L1.removeLast();
 
+ //printing new updated linkedlist L1
 System.out.println(L1);
 }}
 
