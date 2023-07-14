6bd9dc6e5bb62a46a97e7a3529979864909a584d
Wrundali shende <118574986+Wrundali31@users.noreply.github.com>
2023-07-15 01:29:15 +0530

    Update and rename Deletestack.java to Delete_stack.java

--- Delete_stack.java
+++ Delete_stack.java
@@ -1,12 +1,17 @@
+//program to delete elements from stack
 import java.util.*;
-class Deletestack
+class Delete_stack
 {
 public static void main(String args[])
 {
+
+//creating stack and inserting elements  
 Stack<String>S1=new Stack<>();
 S1.push("10");
 S1.push("20");
 S1.push("30");
+
+//print stack
 System.out.println(S1);
 String x;
 x=S1.peek();
